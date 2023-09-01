aa477ab7e67f53897414ef99f2a677036c6f95e8
IvanG8 <golikov.vanya@list.ru>
2023-09-01 16:19:31 +0300

    finish

--- controller.js
+++ controller.js
@@ -1,34 +1,37 @@
-const readlineSync = require('readline-sync');
-const fs = require('fs');
-const { EOL } = require('os');
-const chalk = require('chalk') 
+const readlineSync = require("readline-sync");
+const fs = require("fs");
+const { EOL } = require("os");
+const chalk = require("chalk");
+const player = require("play-sound")((opts = {}));
 
-console.log(chalk.bgGreen('hello'))
 const userName = readlineSync.question(`Введите Имя\n`);
-console.log('Привет ' + userName + '!');
-console.log('Хорошей игры!');
+console.log("Привет " + userName + "!");
+player.play("./src/sounds/win.mp3", function (err) {
+  if (err) throw err;
+});
+console.log("Хорошей игры!");
 
 function playTheGame() {
-  let enots = fs.readFileSync('./topics/animals.txt', 'utf-8');
-  let plan = fs.readFileSync('./topics/computers.txt', 'utf-8');
-  let ptices = fs.readFileSync('./topics/funny.txt', 'utf-8');
+  let enots = fs.readFileSync("./topics/animals.txt", "utf-8");
+  let plan = fs.readFileSync("./topics/computers.txt", "utf-8");
+  let ptices = fs.readFileSync("./topics/funny.txt", "utf-8");
 
-  let animals = ['animals', 'computers', 'funny'],
-  index = readlineSync.keyInSelect(animals, 'Выберите тему');
+  let animals = ["animals", "computers", "funny"],
+    index = readlineSync.keyInSelect(animals, "Выберите тему");
   let result1 = [];
 
-  if (animals[index] == 'animals') {
-    let result = enots.split(EOL).filter((el) => el != '');
+  if (animals[index] == "animals") {
+    let result = enots.split(EOL).filter((el) => el != "");
     for (let i = 0; i < result.length; i = i + 2) {
       result1.push([result[i], result[i + 1]]);
     }
-  } else if (animals[index] == 'computers') {
-    let result = plan.split(EOL).filter((el) => el != '');
+  } else if (animals[index] == "computers") {
+    let result = plan.split(EOL).filter((el) => el != "");
     for (let i = 0; i < result.length; i = i + 2) {
       result1.push([result[i], result[i + 1]]);
     }
-  } else if (animals[index] == 'funny') {
-    let result = ptices.split(EOL).filter((el) => el != '');
+  } else if (animals[index] == "funny") {
+    let result = ptices.split(EOL).filter((el) => el != "");
     for (let i = 0; i < result.length; i = i + 2) {
       result1.push([result[i], result[i + 1]]);
     }
@@ -37,21 +40,40 @@ function playTheGame() {
   let count = 0;
   for (let i = 0; i < result1.length; i++) {
     userName = readlineSync.question(`${result1[i][0]}\n`);
-    if (userName === `${result1[i][1]}`) { 
-        console.log(chalk.blue`Правильно 👍`)
-      count +=1
+    if (userName === `${result1[i][1]}`) {
+      player.play("./src/sounds/hands.mp3", function (err) {
+        if (err) throw err;
+      });
+      console.log(chalk.blue`Правильно 👍`);
+      count += 1;
     } else {
-      console.log(chalk.bgRed('Вы отвечаете не правильно!!'));
-      count = count -1 
-    } 
+      player.play("./src/sounds/looose.mp3", function (err) {
+        if (err) throw err;
+      });
+      console.log(
+        chalk.bgRed(`Вы отвечаете не правильно!!`),
+        chalk.bgGreen(`\nПравильный ответ: ${result1[i][1]}`)
+      );
+      //console.log(chalk.bgRed(`Вы отвечаете не правильно!!\nПравильный ответ: ${result1[i][1]}`));
+      count = count - 1;
+    }
   }
-  console.log('Игра закончена')
-  if(count < result1.length/2){
-    console.log(count)
-    console.log(`¯\\_(ツ)_/¯\n__________\nПлохо`)
-  } else if (count < result1.length/2) {
-  console.log('Вы играете прекрасно!🤓');
-  console.log(`Ваш счет: ${count}`)
-  } 
+  setTimeout(() => {
+    console.log("Игра закончена");
+    if (count < result1.length / 2) {
+      player.play("./src/sounds/victory.mp3", function (err) {
+        if (err) throw err;
+      });
+      console.log(count);
+      console.log(`¯\\_(ツ)_/¯\n__________\nУвы и ах! Какой конфузъ!`);
+    } else if (count < result1.length / 2) {
+      player.play("./src/sounds/victory.mp3", function (err) {
+        if (err) throw err;
+      });
+      console.log("Вы играете прекрасно!🤓");
+      console.log(`Ваш счет: ${count}`);
+      console.log(`¯\\_(ツ)_/¯`);
+    }
+  }, 6000);
 }
 playTheGame();
